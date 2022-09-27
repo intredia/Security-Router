c9295c3fc771a46d8a82f5e836fbc05a7d4beb04
Mashrafi Shahriar <mashrafishahriar@outlook.com>
2022-09-27 20:54:49 +0600

    added certificates

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const https = require('node:https');
-const fs = require('node:fs');
+const https = require('https');
+const fs = require('fs');
 
 
 const options = {
-  key: fs.readFileSync('cert/fixtures/keys/ag-key.pem'),
-  cert: fs.readFileSync('cert/fixtures/keys/ag-cert.pem')
+  key: fs.readFileSync('cert/fixtures/keys/ag_org.key'),
+  cert: fs.readFileSync('cert/fixtures/keys/ag_org.crt')
 };
-
 https.createServer(options, (req, res) => {
   res.writeHead(200);
   res.end('hello world\n');
 }).listen(8000);
+
+
 
